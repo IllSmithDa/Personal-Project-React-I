7a6698483da69b27c7f8e796e55d7ba2ed6682bd
Sam Kim <sams404link@gmail.com>
2017-10-24 17:59:03 -0700

    updated vidhost

--- vidhost/src/App.js
+++ vidhost/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './HomePage.css';
 
 class App extends Component {
   constructor () {
@@ -11,16 +12,36 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1> Vidhost</h1>
+      <body>
+        <h1 class = "HomePage__header"> Vidhost</h1>
         <br></br>
-        <label for = "username"> Username/Email: </label>
+        <label for = "username"> Username/Email </label>
+        <br></br>
         <input type = "text" id = "username"/>
         <br></br>
         <br></br>
-        <label for = "password"> Password: </label>
+        <label for = "password"> Password </label>
+        <br></br>
         <input type = "text" id = "password"/>
-      </div>
+        <br></br>
+        <br></br>
+        <button> Submit </button>
+        <h2 class = "HomePage__header"> Most Popular Videos </h2> 
+        <br></br>
+        <div>
+        <img src = {require('./test.jpg')} width = "200" height = "200" alt = ""/>
+        <br/>
+        <p2 > Gold PS4 Unboxing!!! </p2>
+        </div>
+        <img src = {require('./test2.jpg')} class = "App-right" width = "200" height = "200" alt = ""/> <tab/>
+        <img src = {require('./GiantDot.png')} width = "200" height = "200" alt = ""/> <tab/>
+        <img src = {require('./Planet1.png')} width = "200" height = "200" alt = ""/> <tab/>
+        <img src = {require('./Ship1.png')} width = "200" height = "200" alt = ""/> <tab/>
+        <img src = {require('./ground1.png')} width = "200" height = "200" alt = ""/> <tab/>
+        <img src = {require('./grass1.png')} width = "200" height = "200" alt = ""/> <tab/>
+        <img src = {require('./test3.png')} width = "200" height = "200" alt = ""/> <tab/>
+        <img src = {require('./tower1.png')} width = "200" height = "200" alt = ""/> <tab/>
+      </body>
     );
   }
 }
